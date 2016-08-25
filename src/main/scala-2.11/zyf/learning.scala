ddd1fc9b8003d477041c8555b0db99ffad8b69b1
Zhong <zhongyifan@neotel.com.cn>
2016-08-25 23:49:58 +0800

    curry

--- src/main/scala-2.11/zyf/learning.scala
+++ src/main/scala-2.11/zyf/learning.scala
@@ -56,6 +56,20 @@ object Learning {
 
   }
 
+  /**
+    *2.3 2.4 2.5
+    */
+  object Curry {
+    def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+      (a: A) => ((b: B) => f(a, b))
+
+    def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+      (a: A, b:B) => f(a)(b)
+
+    def compose[A, B, C](f: B => C, g: A => B) : A => C =
+      (a: A) => f(g(a))
+  }
+
   def main(args: Array[String]) {
     println(Fibonacci.fib(3))
 
