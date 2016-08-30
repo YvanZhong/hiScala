6758aead641a4a5eddcba65951a41395b7b13138
Zhong <zhongyifan@neotel.com.cn>
2016-08-30 23:26:19 +0800

    fold

--- src/main/scala-2.11/zyf/learning.scala
+++ src/main/scala-2.11/zyf/learning.scala
@@ -70,10 +70,33 @@ object Learning {
       (a: A) => f(g(a))
   }
 
+  /**
+    *3.12
+    */
+  object Datastructures {
+
+    def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+      case Nil => z
+      case h :: t => foldLeft(t, f(z,h))(f)
+    }
+
+
+    def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = as match {
+      case Nil => z
+      case x :: xs => f(x, foldRight(xs, z)(f))
+    }
+
+    def reverse[A](l: List[A]): List[A] = foldLeft(l, List[A]())((acc,h) => h :: acc)
+//    def reverse[A](l: List[A]): List[A] = foldRight(l, List[A]())((h,acc) => h :: acc)
+
+  }
+
   def main(args: Array[String]) {
     println(Fibonacci.fib(3))
 
     println(Order.isSorted(Array(1, 3, 3), (a: Int, b: Int) => a < b))
+
+    println(Datastructures.reverse(List(1, 2, 3)))
 
   }
 }
